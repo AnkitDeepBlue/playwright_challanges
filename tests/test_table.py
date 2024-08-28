70731eeecbea8205de056755317e57ee7fd565a7
AnkitTripathiPersonal <ankit.deepblue@gmail.com>
2024-08-28 22:53:00 +0530

    updates

--- tests/test_table.py
+++ tests/test_table.py
@@ -1,13 +1,15 @@
 import pytest
 import allure
 
+
 @allure.feature('Table Sorting')
 @allure.description("This is to sort table by columns and assert the sorting")
 @pytest.mark.parametrize("column_name, order", [("Name", "sorting_desc"), ("Age", "sorting_desc")])
 def test_sorting_table_by_column(table_page, column_name, order):
     table_page.load()
     final_sorting_details = table_page.sort_by_column(column_name, order)
-    assert {column_name: order} == final_sorting_details
+    assert {column_name:order} == final_sorting_details
+
 
 @allure.feature('Table Search')
 @allure.description("This is to search the names in table and validate it appears in row")
