dba9b0db3379a360108e61d02c88cd89667acc28
AnkitTripathiPersonal <ankit.deepblue@gmail.com>
2024-08-29 01:35:11 +0530

    updates

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -2,7 +2,7 @@ import pytest
 import allure
 
 
-@pytest.mark('smoke')
+@pytest.mark.smoke
 @allure.feature('This is smoke test')
 @allure.story('Testing smoke story')
 @allure.description("SMOKE TEST")
